01abe48a02bf86830e5c0ee35f29591c4cc58409
谷政烨 <18534132836@126.com>
2021-01-07 16:05:51 +0800

    2021年1月7日16:05:06 学习module

---
+++ workstudy/src/main/java/com/example/workstudy/WorkstudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.workstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkstudyApplication.class, args);
+    }
+
+}
